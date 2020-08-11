2f1ce6d1abee9e4f882da3638e920482579d4d0e
JS <jeremyspk@gmail.com>
2020-08-12 00:17:01 +0800

    Revert "Further experimentation"
    This reverts commit 9834df2a4716cb0a2ba39cef4b06635f379316b6.

--- main.py
+++ main.py
@@ -1,62 +1,28 @@
+
+#debug
+import gym
 import numpy as np 
+import gym_snake2
 import time
+
 from gann_agent import *
 
-#? Testing only debug
-import gym
-import gym_snake3
-
 def main():
-    #? Testing only debug
-    '''
-    env = gym.make('snake3-v0', render=True, segment_width=25, width=20, height=20)
-    env.reset()
-    print("normalized_distance:", env._normalized_distance_two_points((20,20), (19,20)))
+    gann_player = GANNAgent(fitness_top_percentile=0.005, mutation_rate=0.01,learning_rate=1e-3) # 1e-3*5 0.005
 
-    print("detect wall:", env._detect_object_type_at_a_point((0,1)))
-    print("detect body:", env._detect_object_type_at_a_point(env.snake_segments[1]))
-    print("detect apple:", env._detect_object_type_at_a_point(env.apple_pos))
-    print("detect hopefully nothing:", env._detect_object_type_at_a_point((18,18))) # (19,19) is wall
-    print("detect wall:", env._detect_object_type_at_a_point((1,1))) # Not wall, (0,0) is wall
-    print("")
-    
-
-    env.apple_pos = (5,9)
-    env.render()
-    #print('sweep up:', env._sweep_direction_for_object(env.snake_segments[0], (0,-1)))
-    #print('sweep top-right:', env._sweep_direction_for_object(env.snake_segments[0], (1,-1)))
-    #print('sweep down:', env._sweep_direction_for_object(env.snake_segments[0], (0,1)))
-    #print('sweep down', env._sweep_direction_for_object(env.snake_segments[0], (0,1)))
-    print('_sense_all_directions():', env._sense_all_directions(env.snake_segments[0]))
-    input()
-    return
     '''
-
-    #? Apple Spawn testing
-    '''
-    env = gym.make('snake3-v0', render=True, segment_width=25, width=20, height=20)
-    env.reset()
-    env.render()
-    #env.snake_segments = [(5,5), (5,6), (5,7), (5,8), (5,9), (5,10), (5,11), (5,12), (6,12),(6,11),(7,11),(8,11), (9,11), (10,11)]
-    #env._spawn_apple()
-    
-    print("env.snake_segments:",env.snake_segments)
-    input()
-    return
-    '''
-    #* Initialize GANN Agent
-    gann_player = GANNAgent(fitness_top_percentile=0.005, mutation_rate=0.00,learning_rate=1e-3/2) # 1e-3*5 0.005
-    
     #* Human trained (visual testing of DNN model)
-    #human_model = gann_player.human_training_data(num_of_epoch=30,verbose=True)
+    human_model = gann_player.human_training_data(num_of_epoch=3,verbose=True)
 
-    # Load past cumulative list of training data from directory 
-    human_model = gann_player.load_cumulative_training_data_and_model_fit('cumulative_training/', num_of_epoch=25)
+    # Load past model from file
+    #human_model = gann_player.load_training_data_and_model_fit(filename='human_training_data_simple.npy', num_of_epoch=10)
 
-    gann_player.play(num_of_games=10, frequency=200, model=human_model, random_game=False)
-    
+
+    gann_player.play(num_of_games=5, model=human_model)
     '''
-    #* Genetic Algorithm Training!
+
+
+    #* GANN
     watch_every = 1
 
     for i in range(500):
@@ -64,7 +30,36 @@ def main():
         
         if ((i+1) % watch_every) == 0:
             gann_player.play(num_of_games=5)
-    '''
     
+    #!program prematurely ends here
+    return
+
+    #time.sleep(2)
+    #gann_player.play(random_game=True)
+
+
+
+
+# Test snake v2 env
+'''
+env = gym.make('snake-v1', render=True, segment_width=30)
+env.reset()
+FREQUENCY = 10
+
+def main():
+
+    done = False
+    while(True):
+        print(env._get_board())
+        print("")
+        env.render()
+        
+        _, _, done, _ = env.step(env.action_space.sample())
+        time.sleep(1/FREQUENCY)
+
+        if done:
+            env.reset()
+'''
+
 if __name__ == "__main__":
     main()
